d8f81aa00afc40c2a1da816405777beaf71d6791
Gustavo Restrepo <gustavo.restrepo@utp.edu.co>
2019-07-25 20:49:11 -0500

    Arreglado

--- server/server.js
+++ server/server.js
@@ -3,21 +3,15 @@ require('./database');
 
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-
 
 const port = process.env.PORT;
-
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-
 
 //Configuracion global de routas
 app.use(require('./routes/index'));
-
 
 app.listen(port, () => {
     console.log(`Escuchando puerto ${port}`);
